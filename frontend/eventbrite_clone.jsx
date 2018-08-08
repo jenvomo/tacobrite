7731d5209b9e1a11555ccd050bd958ba7c98b020
jenkennedy <kennifer09@gmail.com>
2018-08-07 18:01:16 -0700

    Create session api util

--- frontend/eventbrite_clone.jsx
+++ frontend/eventbrite_clone.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Eventbrite Clone!!!!</h1>, root)
 });
