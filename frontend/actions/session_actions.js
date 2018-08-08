1a6f8a1d355cd792307130684bab4cadaa3173b8
jenkennedy <kennifer09@gmail.com>
2018-08-07 20:45:32 -0700

    Create session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,36 @@
+import * as SessionApiUtil from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const login = user => dispatch => (
+  SessionApiUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+      errors => dispatch(receiveErrors(errors)))
+);
+
+export const logout = () => dispatch => (
+  SessionApiUtil.logout()
+    .then(() => dispatch(logoutCurrentUser()),
+      errors => dispatch(receiveErrors(errors)))
+);
+
+export const signup = user => dispatch => (
+  SessionApiUtil.signup(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+      errors => dispatch(receiveErrors(errors)))
+);
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  user: currentUser
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
