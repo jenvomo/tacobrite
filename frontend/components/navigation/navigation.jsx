a2a560e91425b96619e32d0d6715a650fbd1dbb3
jenkennedy <kennifer09@gmail.com>
2018-08-09 11:53:48 -0700

    Add logo to navbar

--- frontend/components/navigation/navigation.jsx
+++ frontend/components/navigation/navigation.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 const Navigation = ({ currentUser, logout }) => {
   const sessionLinks = () => (
     <section className="main-nav">
-      <div>Eventliter</div>
-        <ul className='main-nav-options'>
-          <li>Browse Events</li>
-          <li>Help</li>
-          <Link  className='nav-link'to='/login'>Sign In</Link>
-        </ul>
+      <img src={window.images.logo}></img>
+      <ul className='main-nav-options'>
+        <li>Browse Events</li>
+        <li>Help</li>
+        <Link  className='nav-link'to='/login'>Sign In</Link>
+      </ul>
     </section>
   );
 
   const personalGreeting = () => (
     <section className="main-nav">
-      <div>Eventliter</div>
+      <img src={window.images.logo}></img>
       <ul className='main-nav-options'>
         <li>Browse Events</li>
         <Link className='nav-link' to='/event/new'>Create Event</Link>
