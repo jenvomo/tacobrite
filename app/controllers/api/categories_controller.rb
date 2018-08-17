80f085c2b7f9e96dab95dadfbf77bd1e3d763689
jenkennedy <kennifer09@gmail.com>
2018-08-17 14:18:05 -0700

    Fix heroku bug in categories controller

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -2,9 +2,9 @@ class Api::EventsController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      render: "/api/category/show"
+      render "/api/category/show"
     else
-      render: json: @category.errors.full_messages, status: 422
+      render json: @category.errors.full_messages, status: 422
     end
   end
 
