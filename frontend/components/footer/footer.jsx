054542b50a9d99811c041bc000bd0666f3a84b39
jenkennedy <kennifer09@gmail.com>
2019-02-17 10:51:41 -0800

    Add developer info and links to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Footer = state => {
   return (
     <div className='footer'>
+      <div className='footer-top'>
+
+        <div className="find-events">
+          <h1>Find Events</h1>
+          <Link className="links" to={`/events`}>Browse Events</Link>
+        </div>
+
+        <div className="meet-developer">
+          <h1>Meet the Developer</h1>
+
+          <div className="dev-links">
+            <a href="https://www.linkedin.com/in/meetjenniferkennedy/">
+              <img src={window.images.linkedin}></img>
+            </a>
+            <a href="https://github.com/jenkennedy">
+              <img src={window.images.github}></img>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="site-basics">
+        <img src={window.images.logo_footer}></img>
+        <text>2018 Eventliter</text>
+      </div>
     </div>
   );
 };
