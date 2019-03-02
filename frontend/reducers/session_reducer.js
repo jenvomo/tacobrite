26b4cac54027b58ee79f465b3b9f1d0aa031152a
jenkennedy <kennifer09@gmail.com>
2019-03-02 10:21:06 -0800

    Remove lodash

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -2,16 +2,15 @@ import {
   RECEIVE_CURRENT_USER,
   LOGOUT_CURRENT_USER
 } from '../actions/session_actions';
-import { merge } from 'lodash';
 
 const sessionReducer = (state = {id: null}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return merge({}, state, {id: action.user.id});
+      return Object.assign({}, state, {id: action.user.id});
     case LOGOUT_CURRENT_USER:
-      return merge({}, state, {id: null});
+      return Object.assign({}, state, {id: null});
     default:
       return state;
   };
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,15 +1,14 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { RECEIVE_EVENT } from '../actions/event_actions';
-import { merge } from 'lodash';
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return merge({}, state, { [action.user.id]: action.user });
+      return Object.assign({}, state, { [action.user.id]: action.user });
     case RECEIVE_EVENT:
-      const nextState = merge({}, state);
+      const nextState = Object.assign({}, state);
       if (Object.values(nextState)[0]) {
         const eventArray = Object.values(nextState)[0].myEvents;
   
